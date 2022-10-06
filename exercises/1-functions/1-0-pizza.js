f7e16d27ac82b5d6733d5470eb67f9555f3ca96d
elke hoste <elke.hoste@student.ehb.be>
2022-10-06 18:17:39 +0200

    added a canvas and drew an orange

--- exercises/1-functions/1-0-pizza.js
+++ exercises/1-functions/1-0-pizza.js
@@ -1,1 +1,17 @@
 "use strict";
+
+window.onresize = drawOrange;
+
+drawOrange();
+
+function drawOrange(){
+    let canvas = document.querySelector("canvas");
+    canvas.width = window.innerWidth;
+    canvas.heigth = window.innerHeight;
+    let context = canvas.getContext("2d");
+    
+    context.beginPath();
+    context.fillStyle :"Orange";
+    context.arc(100, 100, 50, 0, Math.PI * 2);
+    context.fill();
+}
